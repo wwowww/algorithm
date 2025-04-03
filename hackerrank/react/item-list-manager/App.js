d3f73430068c88b6f0ca39cd3e6305125d7f4656
wwowww <105021733+wwowww@users.noreply.github.com>
2025-04-03 21:20:50 +0900

    docs. Add solution for React coding test practice

---
+++ hackerrank/react/item-list-manager/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "h8k-components";
+
+import "./App.css";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddItem = () => {
+    if(input.trim() !== "") {
+      setItems([...items, input]);
+      setInput("");
+    }
+  };
+
+  return (
+    <>
+      <h8k-navbar header="Item List Manager"></h8k-navbar>
+      <div className="App">
+        <h3>Item List</h3>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter item"
+          data-testid="input-field"
+        />
+        <button onClick={handleAddItem} data-testid="add-button">
+          Add Item
+        </button>
+        <ul data-testid="item-list">
+          {items.map((item, index) => (
+            <li key={index} data-testid="list-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
